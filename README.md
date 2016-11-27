09fe7e047c0443cef6df9ec5eb63f460a90d22c9
Frode Aannevik <frode.aannevik@dsafe.no>
2016-11-27 17:52:49 +0100

    Scrape trending repos as markdown

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ Archive of the GitHub daily trending git repositories
 
 The repository follows:
 
- - go
- - java
- - javascript
- - python
+ - [go](https://github.com/trending?l=go)
+ - [java](https://github.com/trending?l=java)
+ - [javascript](https://github.com/trending?l=javascript)
+ - [python](https://github.com/trending?l=python)
 
+## Install
+
+Add ``bin/archive-trending`` to your crontab.
+
+    crontab -l | { cat; echo "0 0 * * * $(pwd)/archive-trending"; } | crontab -
---
+++ scrape_trending/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from BeautifulSoup import BeautifulSoup
+
+soup = BeautifulSoup(sys.stdin.read())
+items = soup.findAll(
+    'li', {'class': lambda x: x and 'd-block' in x.split()})
+
+trending = [('https://github.com%s' % li.h3.a.get('href'),
+             li.h3.a.get("href")[1:],
+             li.p.text.rstrip().lstrip().split("\n")[0] if li.p else '')
+            for li in items]
+
+for t in trending:
+    print((u'* [%s](%s): %s' % t).encode('utf-8'))
