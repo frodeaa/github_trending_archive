aa76cf63c03eb62092b30f1aa34ada93ba4f16ed
Frode Aannevik <frode.aannevik@dsafe.no>
2016-11-27 23:44:04 +0100

    Simplify, replace curl with requests

--- scrape_trending/__main__.py
+++ scrape_trending/__main__.py
@@ -1,16 +1,47 @@
 # -*- coding: utf-8 -*-
 import sys
 
+import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
 from BeautifulSoup import BeautifulSoup
 
-soup = BeautifulSoup(sys.stdin.read())
-items = soup.findAll(
-    'li', {'class': lambda x: x and 'd-block' in x.split()})
+__attrs = {
+    'class': lambda x: x and 'd-block' in x.split()
+}
 
-trending = [('https://github.com%s' % li.h3.a.get('href'),
-             li.h3.a.get("href")[1:],
-             li.p.text.rstrip().lstrip().split("\n")[0] if li.p else '')
-            for li in items]
 
-for t in trending:
-    print((u'* [%s](%s): %s' % t).rstrip().encode('utf-8'))
+def trending(content):
+    soup = BeautifulSoup(content)
+    for li in soup.findAll('li', __attrs):
+        yield (li.h3.a.get('href'),
+               li.h3.a.get("href")[1:],
+               "".join(li.p.text.strip().split('\n')) if li.p else '')
+
+
+def markdown(trends, lang):
+    fmt = u'* [https://github.com%s](%s): %s'
+    print((u'\n####%s' % lang).encode('utf-8'))
+    for t in trends:
+        print((fmt % t).rstrip().encode('utf-8'))
+
+
+def main(argv):
+    s = requests.Session()
+    s.headers.update({
+        'User-Agent': 'python:github_trending_archive (by /frodeaa)'
+    })
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    status_forcelist=[500, 502, 503, 504])
+    s.mount('https://github.com/trending',
+            HTTPAdapter(max_retries=retries))
+
+    for lang in argv:
+        url = 'https://github.com/trending?l=%s' % lang
+        markdown(trending(s.get(url).text), lang)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
